8c853d98d172e8fa2a3baa633eb85ffb6e0bd2e8
Bao Nguyen <nqb9811@gmail.com>
2025-10-30 22:19:12 +0700

    make npm pkg

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { AbortException } from './abort-exception';
+export { Task, TaskResult, WorkerTaskHandler } from './worker-task-descriptor';
+export { initWorker } from './init-worker';
+export { WorkerPool } from './worker-pool';
